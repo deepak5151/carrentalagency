6eed372cea328dfe8b2f829f82fd298684f85faa
Deepak Kapoor <nk93555@gmail.com>
2020-05-08 13:03:41 +0530

    Updates

---
+++ precache-manifest.c7e52fd8825db2472391b0a8d5d09888.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37a3f61f06ca8f59b308a700e2de986e",
+    "url": "/carrentalagency/index.html"
+  },
+  {
+    "revision": "fa96e0dd85424ae984ff",
+    "url": "/carrentalagency/static/css/2.097832d3.chunk.css"
+  },
+  {
+    "revision": "0cf6866e2ed16364bb0e",
+    "url": "/carrentalagency/static/css/main.39a668e1.chunk.css"
+  },
+  {
+    "revision": "fa96e0dd85424ae984ff",
+    "url": "/carrentalagency/static/js/2.8ef4ebd4.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/carrentalagency/static/js/2.8ef4ebd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cf6866e2ed16364bb0e",
+    "url": "/carrentalagency/static/js/main.dcf680f5.chunk.js"
+  },
+  {
+    "revision": "a684b2d3412b5d27321a",
+    "url": "/carrentalagency/static/js/runtime-main.5ad64daa.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/carrentalagency/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/carrentalagency/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/carrentalagency/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/carrentalagency/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/carrentalagency/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
